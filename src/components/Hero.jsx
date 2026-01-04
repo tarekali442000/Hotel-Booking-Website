74f93f56dead59d8cb4bd73f6aae8a38c3279a85
Tarek Ali <tarek.ali.hamed.amr@gmail.com>
2026-01-04 16:43:58 +0200

    Landing page V2.1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 
     text-white bg-[url("/src/assets/heroImage.png")] bg-no-repeat bg-center bg-cover h-screen' > 
-      <p className='bg-[#49B9FF]/50 px-3.5 py-1 rounded-full mt-30'>The Ultimate Hotel Experience</p>
+      <p className='bg-[#49B9FF]/50 px-3.5 py-1 rounded-full mt-20'>The Ultimate Hotel Experience</p>
       <h1 className='font-playfair text-2xl md:text-5xl md:text-[56px] md:leading-[56px] font-bold md:font-extrabold max-w-xl mt-4'>Discover Your Perfect Gateway Destination</h1>
       <p className='max-w-130 mt-2 text-sm md:text-base'>Unparalleled luxury and comfort await at the world's most exclusive hotels and resorts. Start your journey today.</p>
     <form className='bg-white text-gray-500 rounded-lg px-6 py-4 mt-8  flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto'>
