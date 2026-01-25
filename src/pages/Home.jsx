e0632d965f0d8f5dc681d872e632a1faa40e3974
Tarek Ali <tarek.ali.hamed.amr@gmail.com>
2026-01-25 02:48:57 +0200

    finish testmonial section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedDestinatin from '../components/FeaturedDestinatin'
 import ExclusiveOffer from '../components/ExclusiveOffer'
+import Testmonial from '../components/Testmonial'
 
 const Home = () => {
   return (
     <>
-      <Hero/>
-      <FeaturedDestinatin/>
-      <ExclusiveOffer/>
+      <Hero />
+      <FeaturedDestinatin />
+      <ExclusiveOffer />
+      <Testmonial />
     </>
   )
 }
